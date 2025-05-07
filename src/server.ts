58abf11a8cf667b2455a540231cc31bf3bc03b94
Michael Schmidt <m.schmidt@resolution.de>
2025-05-07 13:19:18 +0200

    Changed PORT variable

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const server = createExpressMiddleware({
 
 app.use('/trpc', server);
 
-const PORT = process.env.SERVER_PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, async () => {
     await connectDB();
